ee0aaf8a7def4403b1724e5fe1abe7178dba932b
0xtriboulet <santos.s.salinas@gmail.com>
2023-03-04 23:52:49 -0800

    Tweaking Polymorphism functionality

--- Agent/Include/Config.h
+++ Agent/Include/Config.h
@@ -1,6 +1,6 @@
 #define CONFIG_USER_AGENT L"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"
-#define CONFIG_HOST L"192.168.0.37"
-#define CONFIG_PORT 443
+#define CONFIG_HOST L"127.0.0.1"
+#define CONFIG_PORT 9001
 #define CONFIG_SECURE TRUE
 #define CONFIG_SLEEP 10
 #define CONFIG_UNMAP TRUE
