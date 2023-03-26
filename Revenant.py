b5e9343d50e212f446922c0c89c89a434657400c
0xtriboulet <santos.s.salinas@gmail.com>
2023-03-25 21:23:15 -0700

    Dynamic hashing and XORing at build time

--- Revenant.py
+++ Revenant.py
@@ -15,27 +15,6 @@ COMMAND_DOWNLOAD: int = 0x154
 COMMAND_EXIT: int = 0x155
 COMMAND_OUTPUT: int = 0x200
 
-GENERATED_PASSWORD: str = ''.join(random.choices(string.ascii_letters, k=16))
-
-plain_strings = [
-    "#define SEED 0xDEADDEAD",
-    "#define RtlRandomEx_CRC32B             \"RtlRandomEx\"",
-    "#define RtlGetVersion_CRC32B           \"RtlGetVersion\"",
-    "#define RtlInitUnicodeString_CRC32B    \"RtlInitUnicodeString\"",
-    "#define NtCreateFile_CRC32b            \"NtCreateFile\"",
-    "#define NtQueryInformationFile_CRC32B  \"NtQueryInformationFile\"",
-    "#define NtAllocateVirtualMemory_CRC32B \"NtAllocateVirtualMemory\"",
-    "#define S_XK                     \"PUNICODE_STRING\"",
-    "#define S_INSTANCE_NOT_CONNECTED \"instance not connected!\"",
-    "#define S_COMMAND_NOT_FOUND      \"command not found\"",
-    "#define S_IS_COMMAND_NO_JOB      \"is command no job\"",
-    "#define S_TRANSPORT_FAILED       \"transport failed\"",
-    "#define S_COMMAND_SHELL          \"command shell\"",
-    "#define S_COMMAND_UPLOAD         \"command upload\"",
-    "#define S_COMMAND_DOWNLOAD       \"command download\"",
-    "#define S_COMMAND_EXIT           \"command exit\""
-]
-
 
 GENERATED_PASSWORD: str = ''.join(random.choices(string.ascii_letters, k=16))
 password_bytes = GENERATED_PASSWORD.encode('utf-8')
